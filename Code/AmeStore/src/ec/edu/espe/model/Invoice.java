1816dbed84c8f9f394e00ba3fb5ea05d5030fbe3
Ing. Alex <daullcu@espe.edu.ec>
2024-12-12 11:07:34 -0500

    Evidence Flow

--- Code/AmeStore/src/ec/edu/espe/model/Invoice.java
+++ Code/AmeStore/src/ec/edu/espe/model/Invoice.java
@@ -1,14 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ec.edu.espe.model;
 import java.util.ArrayList;
 import java.util.List; 
 import java.util.Scanner;
 /**
  *
- * @author USER
+ * @author TEAM TNT
  */
 public class Invoice {
      private String customerName;
