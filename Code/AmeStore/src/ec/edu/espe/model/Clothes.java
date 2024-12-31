ed19179f6ba81cb8f9d9a7994a3aaebbcee3ed81
Ing. Alex <daullcu@espe.edu.ec>
2024-12-30 19:04:13 -0500

    inheritance

--- Code/AmeStore/src/ec/edu/espe/model/Clothes.java
+++ Code/AmeStore/src/ec/edu/espe/model/Clothes.java
@@ -1,99 +1,75 @@
 package ec.edu.espe.model;
 
 /**
- *
- * @author Team TNT
+ * Derived class for specific clothes
  */
-public class Clothes {
+public class Clothes extends ClothingItem {
     private String idProduct;
-    private String name;
     private String category;
     private String size;
     private String color;
-    private double price;
     private int amount;
 
-    @Override
-    public String toString() {
-        return "Clothes{" + "idProduct=" + idProduct + ", name=" + name + ", category=" + category + ", size=" + size + ", color=" + color + ", price=" + price + ", amount=" + amount + '}';
-    }
-
     public Clothes(String idProduct, String name, String category, String size, String color, double price, int amount) {
+        super(name, price); // Llama al constructor de la clase padre
         this.idProduct = idProduct;
-        this.name = name;
         this.category = category;
         this.size = size;
         this.color = color;
-        this.price = price;
         this.amount = amount;
     }
 
-   
+    // Getters and setters
     public String getIdProduct() {
         return idProduct;
     }
 
-    
     public void setIdProduct(String idProduct) {
         this.idProduct = idProduct;
     }
-
-    public String getName() {
-        return name;
-    }
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
 
     public String getCategory() {
         return category;
     }
 
-
     public void setCategory(String category) {
         this.category = category;
     }
-
 
     public String getSize() {
         return size;
     }
 
-
     public void setSize(String size) {
         this.size = size;
     }
-
 
     public String getColor() {
         return color;
     }
 
-
     public void setColor(String color) {
         this.color = color;
     }
-
-
-    public double getPrice() {
-        return price;
-    }
-
- 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
 
     public int getAmount() {
         return amount;
     }
 
-
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Clothes{" +
+                "idProduct='" + idProduct + '\'' +
+                ", category='" + category + '\'' +
+                ", size='" + size + '\'' +
+                ", color='" + color + '\'' +
+                ", amount=" + amount +
+                ", name='" + getName() + '\'' + // Llama a los métodos de la clase padre
+                ", price=" + getPrice() +
+                '}';
     }
 }
--- Code/AmeStore/src/ec/edu/espe/model/ClothingItem.java
+++ Code/AmeStore/src/ec/edu/espe/model/ClothingItem.java
@@ -1,15 +1,31 @@
 package ec.edu.espe.model;
 
 /**
- *
- * @author Team TNT
+ * Autor Alexander Ullco
  */
 public class ClothingItem {
     private String name;
-   private double price;
+    private double price;
 
     public ClothingItem(String name, double price) {
         this.name = name;
+        this.price = price;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
@@ -17,33 +33,4 @@ public class ClothingItem {
     public String toString() {
         return "ClothingItem{" + "name=" + name + ", price=" + price + '}';
     }
-
- 
-    public String getName() {
-        return name;
-    }
-
-  
-    public void setName(String name) {
-        this.name = name;
-    }
-
- 
-    public double getPrice() {
-        return price;
-    }
-
- 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    int getStock() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    boolean reduceStock(int quantity) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
 }
